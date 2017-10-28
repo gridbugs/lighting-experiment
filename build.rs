8bce771fa3ab4853a9a472f87046467fe017409b
Stephen Sherratt <sfsherratt@gmail.com>
2017-10-28 20:03:46 +0100

    New code generation api

--- build.rs
+++ build.rs
@@ -1,10 +1,5 @@
 extern crate entity_store_code_gen;
 
-use entity_store_code_gen::GeneratedCode;
-
 fn main() {
-    GeneratedCode::generate(include_str!("spec.toml"))
-        .expect("Failed to generate code")
-        .save()
-        .expect("Failed to save code");
+    entity_store_code_gen::generate(include_str!("spec.toml")).unwrap()
 }
